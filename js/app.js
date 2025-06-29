faaf777e27183d82f9fdad517d10e0757781f041
Yusuf qamber <yousif02.qamber@gmail.com>
2025-06-29 14:13:56 +0300

    adding a README file

--- js/app.js
+++ js/app.js
@@ -9,9 +9,12 @@ let computerChoice
 
 
 /*------------------------ Cached Element References ------------------------*/
-const rockButton= document.querySelector("#Rock")
-const PaperButton=document.querySelector("#Paper");
-const ScissorsButton=document.querySelector("#Scissors")
+const rockButton= document.querySelector("#rock")
+const PaperButton=document.querySelector("#paper");
+const ScissorsButton=document.querySelector("#scissors")
+
+const resultDisplay=document.querySelector("#result-display")
+const resetButton=document.querySelector(".game-button")
 
 // console.log(rockButton,PaperButton);
 
@@ -23,19 +26,51 @@ function getComputerChoice(){
   //generate a random number 0-2
   // select the item from the array
   const randomIndex= Math.floor(Math.random()*choice.length)
-  console.log("randomIndex",randomIndex)
+  // console.log("randomIndex",randomIndex)
   return choice[randomIndex]
 }
 
-function play(){
+function play(event){
 computerChoice=getComputerChoice()
- console.log("computerChoice",computerChoice)
-  render()
+
+ playerChoice = event.target.id
+//  console.log(playerChoice)
+ compare()
+ render()
 }
 
+
+
+function compare(){
+if(playerChoice==="rock"&&computerChoice==="scissors")
+  msg="you wins!"
+
+else if(playerChoice==="paper"&&computerChoice==="rock")
+    msg="you wins!"
+else if(playerChoice==="scissors"&&computerChoice==="paper")
+    msg="you wins!"
+else if(playerChoice===computerChoice)
+    msg="you Tie!"
+  else 
+  msg="you lose!"
+
+
+}
+
+//update UI 
 function render(){
+  
+resultDisplay.textContent = `Computer chose ${computerChoice} and you chose ${playerChoice}. ${msg}`
 
 }
 
+function resetGmae(){
+  resultDisplay.textContent=""
+  computerChoice=null
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
+rockButton.addEventListener("click",play)
+PaperButton.addEventListener("click",play)
+ScissorsButton.addEventListener("click",play)
+resetButton.addEventListener("click",resetGmae)
