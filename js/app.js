4b28c68e90f42b529a709d00c8714501812a8fe8
Yusuf qamber <yousif02.qamber@gmail.com>
2025-06-29 11:10:09 +0300

    creat play ,render and get computerchoice functions

--- js/app.js
+++ js/app.js
@@ -13,13 +13,29 @@ const rockButton= document.querySelector("#Rock")
 const PaperButton=document.querySelector("#Paper");
 const ScissorsButton=document.querySelector("#Scissors")
 
-console.log(rockButton,PaperButton);
+// console.log(rockButton,PaperButton);
 
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+function getComputerChoice(){
+  //generate a random number 0-2
+  // select the item from the array
+  const randomIndex= Math.floor(Math.random()*choice.length)
+  console.log("randomIndex",randomIndex)
+  return choice[randomIndex]
+}
 
+function play(){
+computerChoice=getComputerChoice()
+ console.log("computerChoice",computerChoice)
+  render()
+}
+
+function render(){
+
+}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
