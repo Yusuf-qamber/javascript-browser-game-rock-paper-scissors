cd14402d031800c12510fd92f8a6b9d5738858cb
Yusuf qamber <yousif02.qamber@gmail.com>
2025-06-29 10:45:58 +0300

    add variables and and a constantan

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("working!")
+/*-------------------------------- Constants --------------------------------*/
+const choice=["rock","paper","scissors"]
+
+
+/*-------------------------------- Variables --------------------------------*/
+let msg
+let playerChoice
+let computerChoice
+
+
+/*------------------------ Cached Element References ------------------------*/
+const rockButton= document.querySelector("#Rock")
+const PaperButton=document.querySelector("#Paper");
+const ScissorsButton=document.querySelector("#Scissors")
+
+console.log(rockButton,PaperButton);
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
